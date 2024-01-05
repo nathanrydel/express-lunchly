76513ceab4a7f81bf831d7f864534c14deb73c83
Nathan Rydel <nathanrydel@gmail.com>
2024-01-05 15:56:39 -0700

    added TODO to optimize getByName classmethod

--- models/customer.js
+++ models/customer.js
@@ -60,6 +60,7 @@ class Customer {
   /**get a customer by first name and last name */
 
   static async getByName(firstName, lastName) {
+    // TODO: Not flexible search, needs 2 and only 2 params to function
     firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
     lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
 
